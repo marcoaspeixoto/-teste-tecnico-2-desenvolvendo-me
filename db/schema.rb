cbe59a3d00b5d19d075a4ad7f0305803b35e6921
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-06-19 15:20:28 -0300

    feat(event): migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_19_181751) do
+ActiveRecord::Schema.define(version: 2023_06_19_181943) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
