8354fb0a99e40c12ef72f42c94c59350f5408b8d
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-07-08 15:56:22 -0300

    test(user): added user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:participations) }
+  it { should have_many(:events).through(:participations) }
+
+  it "creates a new user" do
+    user = create(:user)
+    expect(user.name).to be_present
+    expect(user.email).to be_present
+  end
 end
