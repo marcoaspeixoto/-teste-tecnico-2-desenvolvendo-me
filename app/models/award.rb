c76925ef0e8ec62b5bd17ec896361b3cd3ce83bd
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-08-07 18:54:57 -0300

    refactor(award): spell check

--- app/models/award.rb
+++ app/models/award.rb
@@ -5,6 +5,6 @@ class Award < ApplicationRecord
   private
 
   def prize_limit
-    errors.add(:base, "O evento ja atingiu o limit maximo de premios (3).") if award.maximum_prize_limit?(event)
+    errors.add(:base, "O evento já atingiu o limite máximo de prêmios (3).") if award.maximum_prize_limit?(event)
   end
 end
