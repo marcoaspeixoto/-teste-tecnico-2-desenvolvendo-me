6a72b2b6abea6253fee6a0c8a2fd25da1ddd1cfe
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-08-08 22:10:57 -0300

    fix(award): validation fix and method

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,10 +1,10 @@
 class Award < ApplicationRecord
   belongs_to :event
-  validates :prize_limit
+  validate :prize_limit
 
   private
 
   def prize_limit
-    errors.add(:base, "O evento já atingiu o limite máximo de prêmios (3).") if award.maximum_prize_limit?(event)
+    errors.add(:base, "O evento já atingiu o limite máximo de prêmios (3).") if Limit.maximum_prize_limit?(event)
   end
 end
