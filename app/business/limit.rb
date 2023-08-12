46ef4215f077fb7cb32094f28c2fae9737f1f44c
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-08-11 21:18:35 -0300

    feat(award): prize cap rule exclusion

--- app/business/limit.rb
+++
@@ -1,5 +0,0 @@
-class Limit
-  def self.maximum_prize_limit?(event)
-    event.awards.count >= 3
-  end
-end
--- app/models/award.rb
+++ app/models/award.rb
@@ -1,10 +1,3 @@
 class Award < ApplicationRecord
   belongs_to :event
-  validate :prize_limit
-
-  private
-
-  def prize_limit
-    errors.add(:base, "O evento já atingiu o limite máximo de prêmios (3).") if Limit.maximum_prize_limit?(event)
-  end
 end
