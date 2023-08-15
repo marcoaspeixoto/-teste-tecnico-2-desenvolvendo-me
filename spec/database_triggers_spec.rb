0c90b2e648936ff7beb8b83ebe400e66bd0d2434
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-08-14 21:55:58 -0300

    test(award): factory-free test

--- spec/database_triggers_spec.rb
+++ spec/database_triggers_spec.rb
@@ -1,20 +1,18 @@
-require 'rails_helper'
+# spec/database_triggers_spec.rb
 
 RSpec.describe "Database Triggers" do
-  describe "awards_check_limit trigger" do
-    let(:event) { create(:event) } # Assuming you have a Factory for events
+  it "does not allow creating more than 3 awards for an event" do
+    event = Event.create(name: "Test Event")
 
-    it "does not allow creating more than 3 awards for an event" do
-      # Create 3 awards for the same event
-      Award.create!(event: event)
-      Award.create!(event: event)
-      Award.create!(event: event)
+    Award.create(event: event)
+    Award.create(event: event)
+    Award.create(event: event)
 
-      # Try to insert a fourth award, which should not be successful
-      fourth_award = Award.new(event: event)
-      fourth_award.save
+    # Attempt to create a fourth award
+    fourth_award = Award.new(event: event)
+    fourth_award.save
 
-      expect(fourth_award.errors.full_messages).to include("O evento ja atingiu o limite maximo de premios (3)")
-    end
+    # Verify that the fourth award was not created
+    expect(Award.count).to eq(3)
   end
 end
