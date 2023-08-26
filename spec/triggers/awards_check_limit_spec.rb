865f967e6280600b9c55180de893670a6ebe57ec
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-08-26 09:07:36 -0300

    test(award): use factory to create event

--- spec/triggers/awards_check_limit_spec.rb
+++ spec/triggers/awards_check_limit_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Awards Check limit" do
   it "does not allow creating more than 3 awards for an event" do
-    event = Event.create(name: "Test Event")
+    event = create(:event)
 
+    # creates 3 awards for the same event
     Award.create(event: event)
     Award.create(event: event)
     Award.create(event: event)
@@ -11,10 +12,10 @@ RSpec.describe "Awards Check limit" do
     # Attempt to create a fourth award
     fourth_award = Award.new(event: event)
 
-    # Use expect { ... }.to raise_error to test if ActiveRecord::StatementInvalid is raised
+    # Using expect { ... }.to raise_error to test if ActiveRecord::StatementInvalid was raised
     expect { fourth_award.save }.to raise_error(ActiveRecord::StatementInvalid)
 
-    # Verify that the fourth award was not created
+    # Check if the fourth award has not been created
     expect(Award.count).to eq(3)
   end
 end
